c60364c0568f81b7945aa08056c26c0c2f361f5d
Matheus de Souza Borges <matheusborgs@gmail.com>
2023-11-09 11:05:54 -0300

    Desafio inicial curso dot net

---
+++ DESAFIOFUNDAMENTOS/Program.cs
@@ -0,0 +1,58 @@
+﻿using DESAFIOFUNDAMENTOS.Models;
+
+
+Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+decimal precoInicial = 0;
+decimal precoPorHora = 0;
+
+Console.WriteLine("Seja bem vindo ao sistema de estacionamento! \n" +
+                    "Digite o preço inicial:");
+precoInicial = Convert.ToDecimal(Console.ReadLine());
+
+Console.WriteLine("Agora digite o preço por hora:");
+precoPorHora = Convert.ToDecimal(Console.ReadLine());
+
+Estacionamento es = new Estacionamento(precoInicial, precoPorHora);
+
+string opcao = string.Empty;
+bool menu = true;
+
+while (menu) 
+{
+    Console.Clear();
+    Console.WriteLine("Digite a sua opção: ");
+    Console.WriteLine("1 - Cadastrar veículo");
+    Console.WriteLine("2 - Remover veículo");
+    Console.WriteLine("3 - Listar veículo");
+    Console.WriteLine("4 - Encerrar");
+
+    switch (Console.ReadLine())
+    {
+        case "1":
+            es.SalvarVeiculo();
+            break;
+
+        case "2":
+            es.ExcluirVeiculo();
+            break;
+            
+        case "3":
+            es.ListarVeiculo();
+            break;
+        
+        case "4":
+            menu = false;
+            break;
+
+        default:
+            Console.WriteLine("Opção inválida");
+            break;
+
+    }  
+    
+    Console.WriteLine("Pressione uma tecla para continuar");
+    Console.ReadLine();
+}
+
+Console.WriteLine("O programa se encerrou");
